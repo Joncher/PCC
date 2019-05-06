06a78c3c1695f120da988564c84b49c4648831a0
John Oncher <johnoncher@gmail.com>
2019-05-06 12:15:25 -0400

    first commit/navbar/amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Button,
+  Container,
+  Dimmer,
+  Header,
+  Image,
+  Segment,
+  Grid,
+  Form,
+  Menu,
+  Sticky
+} from "semantic-ui-react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Container className="App" fluid>
+        <Sticky>
+          <Menu inverted borderless color="blue" className="secondary ">
+            <Menu.Item as="h2">
+              Pastoral Counseling Center of Flushing
+            </Menu.Item>
+
+            <Menu.Menu position="right">
+              <Menu.Item
+                name="editorials"
+                active={activeItem === "editorials"}
+                onClick={this.handleItemClick}
+                as="h4"
+              >
+                Editorials
+              </Menu.Item>
+
+              <Menu.Item
+                name="reviews"
+                active={activeItem === "reviews"}
+                onClick={this.handleItemClick}
+                as="h4"
+              >
+                Reviews
+              </Menu.Item>
+
+              <Menu.Item
+                name="upcomingEvents"
+                active={activeItem === "upcomingEvents"}
+                onClick={this.handleItemClick}
+                as="h4"
+              >
+                Upcoming Events
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </Sticky>
+
+        <Grid fluid={"true"} columns={1}>
+          <Grid.Column>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+            <Button size="huge" fluid>
+              Hey
+            </Button>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 export default App;
